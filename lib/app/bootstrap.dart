ada5d257f46db1cda5ed01093a7a04740a350fe1
sahil06sarode <sahil06as@gmail.com>
2026-04-17 23:01:35 +0530

    feat: Integrate Firebase into the Flutter application
    - Added Google Services plugin to settings.gradle.kts for Firebase integration.
    - Updated firebase.json to include FlutterFire configuration for Android and Dart.
    - Initialized Firebase in bootstrap.dart with the generated Firebase options.
    - Created google-services.json for Firebase project configuration.
    - Added firebase_options.dart to manage Firebase options for different platforms.

--- lib/app/bootstrap.dart
+++ lib/app/bootstrap.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app.dart';
 import 'flavor_config.dart';
+import '../firebase_options.dart';
 
 Future<void> bootstrap({required AppFlavor flavor}) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final flavorConfig = FlavorConfig.fromFlavor(flavor);
   runApp(
     ProviderScope(
